61f853d22886b2ae776565f2bbb21f8274b1444d
kayan510812 <kayan.kabukale@gmail.com>
2026-09-22 11:03:54 +0200

    EQ-3 - fixed use import issue

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\MfaController;
+use App\Http\Controllers\QuizController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/ping', function () {
@@ -14,13 +16,11 @@ Route::post('/auth/mfa/verify', [AuthController::class, 'verifyMfa']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/mfa/setup', [MfaController::class, 'setup']);
     Route::post('/mfa/verify', [MfaController::class, 'verify']);
-use App\Http\Controllers\QuizController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
 
 Route::middleware('auth:sanctum')->prefix('quiz')->group(function () {
     Route::get('/start', [QuizController::class, 'start']);
