ddaf8dd440ffa42584b5775126dbc41a4d10533f
kayan510812 <kayan.kabukale@gmail.com>
2026-09-22 08:57:27 +0200

    EQ-2 - added cors and user changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,13 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-#[Fillable(['name', 'email', 'password'])]
+/**
+ * @property string $username
+ * @property string $email
+ * @property int $total_score
+ * @property string $roles
+ */
+#[Fillable(['username', 'email', 'password', 'roles'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
